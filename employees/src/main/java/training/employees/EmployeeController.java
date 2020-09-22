bca7954e152e94b181e45413eccba54f2829d341
iviczian <viczian.istvan@gmail.com>
2020-09-22 13:16:45 +0200

    REST get param, variable

--- employees/src/main/java/training/employees/EmployeeController.java
+++ employees/src/main/java/training/employees/EmployeeController.java
@@ -1,11 +1,11 @@
 package training.employees;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -15,8 +15,13 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @GetMapping
-    public List<EmployeeDto> findEmployees() {
-        return employeeService.findEmployees();
+    public List<EmployeeDto> findEmployees(@RequestParam Optional<String> prefix) {
+        return employeeService.findEmployees(prefix);
+    }
+
+    @GetMapping("/{id}")
+    public EmployeeDto findEmployeeById(@PathVariable("id") long id) {
+        return employeeService.findEmployeeById(id);
     }
 
 }
--- employees/src/main/java/training/employees/EmployeeService.java
+++ employees/src/main/java/training/employees/EmployeeService.java
@@ -8,18 +8,45 @@ import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
+//@AllArgsConstructor
 public class EmployeeService {
+
+    private List<Employee> employees =
+            Collections.synchronizedList(
+            new ArrayList<>(List.of(
+            new Employee(1L, "Jack Doe"),
+            new Employee(2L, "John Doe")
+    )));
 
     private ModelMapper modelMapper;
 
-    public List<EmployeeDto> findEmployees() {
+    public EmployeeService(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public List<EmployeeDto> findEmployees(Optional<String> prefix) {
         Type targetListType = new TypeToken<List<EmployeeDto>>() {}.getType();
-        return modelMapper.map(List.of(new Employee(1L, "Jack Doe"),
-                new Employee(2L, "John Doe")),
+        var filtered = employees.stream()
+                .filter(e -> prefix.isEmpty() || e.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
+                .collect(Collectors.toList());
+
+        return modelMapper.map(filtered,
                 targetListType);
+    }
+
+    public EmployeeDto findEmployeeById(long id) {
+        var employee = employees
+                .stream()
+                .filter(e -> e.getId() == id)
+                .findAny()
+            .orElseThrow(() -> new IllegalArgumentException("Employee not found: " + id));
+        return modelMapper.map(employee, EmployeeDto.class);
     }
 }
