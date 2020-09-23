c1d908f9d1dbd93e344cc28ae48c9d2dbf308e09
iviczian <viczian.istvan@gmail.com>
2020-09-23 11:05:32 +0200

    naplózás

--- employees/src/main/java/training/employees/HelloService.java
+++ employees/src/main/java/training/employees/HelloService.java
@@ -1,6 +1,7 @@
 package training.employees;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class HelloService {
 
 //    private String welcome;
@@ -19,6 +21,9 @@ public class HelloService {
     private EmployeeProperties employeeProperties;
 
     public String sayHello() {
+        log.debug("Hello debug");
+        log.info("Hello info");
+
 //        return welcome + LocalDateTime.now();
         return
                 employeeProperties.getMessage1() +
